e41b0cb575f8eb5c54d2c2b3ae7610f8109a6d13
enessaidtatli <enes.tatli@cfn.com.tr>
2020-07-17 11:13:22 +0300

    director sorguları eklendi

--- routes/director.js
+++ routes/director.js
@@ -1,11 +1,58 @@
 const express = require('express');
 const router = express.Router();
 const Director = require('../models/Director');
-
+const mongoose = require('mongoose');
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
+/*router.get('/', (req, res, next) => {
     const promise = Director.find({});
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});*/
+
+router.get('/', (req, res) => {
+    const promise = Director.aggregate([
+        {
+            $lookup: {
+                from: 'movies',
+                localField: '_id',
+                foreignField: 'director_id',
+                as: 'movies'
+            }
+        },
+        {
+            $unwind: {
+                path: '$movies',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $group:{
+                _id:{
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: {
+                    $push: '$movies'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                bio: '$_id.bio',
+                movies: '$movies'
+            }
+        }
+    ]);
+
     promise.then((data) => {
         res.json(data);
     }).catch((err) => {
@@ -29,12 +76,58 @@ router.post('/', (req, res, next) => {
     });
 });
 
-router.get('/:movie_id', (req, res, next) => {
-    const promise = Movie.findById(req.params.movie_id);
-    promise.then((movie) => {
-        if (!movie)
+
+
+router.get('/:director_id', (req, res, next) => {
+    const promise = Director.aggregate([
+        {
+          $match: {
+              '_id': mongoose.Types.ObjectId(req.params.director_id)
+          }
+        },
+        {
+            $lookup: {
+                from: 'movies',
+                localField: '_id',
+                foreignField: 'director_id',
+                as: 'movies'
+            }
+        },
+        {
+            $unwind: {
+                path: '$movies',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $group:{
+                _id:{
+                    _id: '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                movies: {
+                    $push: '$movies'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                bio: '$_id.bio',
+                movies: '$movies'
+            }
+        }
+    ]);
+
+    promise.then((director) => {
+        if (!director)
             next({message: 'The movie was not found!', code: 88});
-        res.json(movie)
+
+        res.json(director)
     }).catch((err) => {
         res.json(err);
     });
